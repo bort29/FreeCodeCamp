c84a4b879eaec7abc10b29398736b6c0e41eeb1b
bort29 <kalinovskiy99@mail.ru>
2025-09-17 22:18:18 +0300

    bs js 1.8

---
+++ add-new-properties-to-a-javascript-object.js
@@ -0,0 +1,14 @@
+** start of script.js **
+
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.bark = 'woof';
+
+
+
+** end of script.js **
+
---
+++ delete-properties-from-a-javascript-object.js
@@ -0,0 +1,16 @@
+** start of script.js **
+
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails;
+// Only change code below this line
+
+
+** end of script.js **
+
