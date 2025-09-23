65fd9a3bf51e98b37afff6ede65fa0027b2b18f8
bort29 <kalinovskiy99@mail.ru>
2025-09-23 22:29:54 +0300

    15

--- 11electing-from-many-options-with-switch-statements.js
+++ 11electing-from-many-options-with-switch-statements.js
@@ -1,9 +1,7 @@
-** start of script.js **
 
 function caseInSwitch(val) {
   let answer = "";
-  // Only change code below this line
-switch (val) {
+ 
   case 1:
     answer = "alpha";
     break;
@@ -17,11 +15,10 @@ switch (val) {
     answer = "delta";
     break;
 }
-  // Only change code above this line
+  
   return answer;
 }
 
 caseInSwitch(1);
 
-** end of script.js **
 
