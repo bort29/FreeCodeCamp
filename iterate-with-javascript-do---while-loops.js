92a85c3b73981897d716c4ea8fa9d6d31c9f7010
bort29 <kalinovskiy99@mail.ru>
2025-09-29 22:46:06 +0300

    bs js 1.11

---
+++ iterate-with-javascript-do---while-loops.js
@@ -0,0 +1,15 @@
+** start of script.js **
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+ myArray.push(i);
+ i++; 
+ } while (i < 10);
+
+
+** end of script.js **
+
