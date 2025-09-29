a55b7460ef8bd894beafe3b4cbe1178ca00132c2
bort29 <kalinovskiy99@mail.ru>
2025-09-29 22:45:50 +0300

    bs js 1.11

---
+++ generate-random-fractions-with-javascript.js
@@ -0,0 +1,18 @@
+** start of script.js **
+
+function randomFraction() {
+
+  // Only change code below this line
+  let nomber = 0 ;
+  
+  while (nomber === 0 ) {
+    
+    nomber = Math.random()
+  }
+  return nomber
+  
+  // Only change code above this line
+}
+
+** end of script.js **
+
---
+++ generate-random-whole-numbers-with-javascript.js
@@ -0,0 +1,8 @@
+** start of script.js **
+
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}
+
+** end of script.js **
+
